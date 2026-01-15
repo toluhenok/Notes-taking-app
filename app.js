e66dc7319ceaa8d85c84825458e4f39c2bbd0359
Toluhenok <adetunjienocht@gmail.com>
2026-01-15 16:45:26 +0100

    added edit and delete notes function

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ function saveNotes() {
     localStorage.setItem('quicknotes', JSON.stringify(notes))
 }
 
+function deleteNote(noteId) {
+    notes = notes.filter(note => note.id !== noteId);
+    saveNotes();
+    renderNotes();
+}
+
 function renderNotes() {
     const notesContainer = document.querySelector('.notes-grid');
     
@@ -50,16 +56,36 @@ function renderNotes() {
         <div class="note-card">
             <h3 class="note-title">${note.title}</h3>
             <p class="note-content">${note.content}</p>
+            <div class="note-actions">
+                <button class="edit-btn" onclick="openNoteDialog('${note.id}')" title="Edit Note">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#191b23"><path d="M184-184v-83.77l497.23-498.77q5.15-5.48 11.07-7.47 5.93-1.99 11.99-1.99 6.06 0 11.62 1.54 5.55 1.54 11.94 7.15l38.69 37.93q5.61 6.38 7.54 12 1.92 5.63 1.92 12.25 0 6.13-2.24 12.06-2.24 5.92-7.22 11.07L267.77-184H184Zm505.15-466.46L744-704.54 704.54-744l-54.08 54.85 38.69 38.69Z"/></svg>
+                </button>
+                <button class="delete-btn" onclick="deleteNote('${note.id}')" title="Delete Note">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#191b23"><path d="M291-267.69 267.69-291l189-189-189-189L291-692.31l189 189 189-189L692.31-669l-189 189 189 189L669-267.69l-189-189-189 189Z"/></svg>
+                </button>
+                    </div>
+
         </div>
         `).join('')
 
 }
 
-function openNoteDialog() {
+function openNoteDialog(noteId) {
     const dialog = document.getElementById('noteDialog');
     const titleInput = document.getElementById('noteTitle');
     const contentInput = document.getElementById('noteContent');
 
+    if (noteId) {
+        const note = notes.find(n => n.id === noteId);
+        if (note) {
+            titleInput.value = note.title;
+            contentInput.value = note.content;
+        }
+    } else {
+        titleInput.value = '';
+        contentInput.value = '';
+    }
+    
     dialog.showModal()
     titleInput.focus()
 }
